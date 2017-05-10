c47cf83ee3add7174f9e1692020726da10261327
Nellie McKesson <nelliemckesson@gmail.com>
2017-05-10 09:30:20 -0400

    adding logging

--- send_to_coresource.rb
+++ send_to_coresource.rb
@@ -5,6 +5,8 @@ require_relative '../bookmaker/core/metadata.rb'
 
 # ---------------------- VARIABLES
 
+local_log_hash, @log_hash = Bkmkr::Paths.setLocalLoghash
+
 csfilename = "#{Metadata.eisbn}_EPUB"
 csfilename = renameFinalEpub(csfilename, stage_dir, 'rename_final_epub_for_firstpass')
 csdir = File.join(Bkmkr::Project.working_dir, "send_to_coresource")
@@ -13,8 +15,12 @@ epubregexp = File.join(Bkmkr::Paths.done_dir, "*.epub")
 # ---------------------- METHODS
 
 ## wrapping a Mcmlln::Tools method in a new method for this script; to return a result for json_logfile
-def localCopyFile(source, dest)
+def localCopyFile(source, dest, logkey='')
   Mcmlln::Tools.copyFile(source, dest)
+  logstring = 'copying to send_to_coresource folder'
+rescue => logstring
+ensure
+    Mcmlln::Tools.logtoJson(@log_hash, logkey, logstring)
 end
 
 # ---------------------- PROCESSES
@@ -22,4 +28,8 @@ end
 # copy all epubs from done dir
 # to coresource_send dir,
 # which then triggers the coresource_connector.rb script in /utilities
-localCopyFile(epubregexp, csdir)
+localCopyFile(epubregexp, csdir, "copyfile")
+
+# Write json log:
+Mcmlln::Tools.logtoJson(@log_hash, 'completed', Time.now)
+Mcmlln::Tools.write_json(local_log_hash, Bkmkr::Paths.json_log)
