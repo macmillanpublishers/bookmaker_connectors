4a3dd12a9823d90d5a3c73d8737174e42e3db1e8
mattretzer <matthew.retzer@macmillan.com>
2017-06-05 11:04:40 -0400

    adding conditional to prevent auto-upload in cases of epubcheck error

--- epub_to_coresourceSend.rb
+++ epub_to_coresourceSend.rb
@@ -9,6 +9,7 @@ local_log_hash, @log_hash = Bkmkr::Paths.setLocalLoghash
 
 csdir = File.join(Bkmkr::Project.working_dir, "send_to_coresource")
 epubregexp = File.join(Bkmkr::Paths.done_dir, Metadata.pisbn, "*.epub")
+epub_errfile = File.join(Bkmkr::Paths.done_dir, Metadata.pisbn, "EPUBCHECK_ERROR.txt")
 
 # ---------------------- METHODS
 
@@ -31,11 +32,16 @@ end
 
 # ---------------------- PROCESSES
 
-# copy all epubs from done dir
-# to coresource_send dir,
-# which then triggers the coresource_connector.rb script in /utilities
-filelist = getFileList(epubregexp, "files_to_copy")
-copyFiles(filelist, csdir, "status")
+# skip coresource upload if epubcheck error-file is present
+unless File.file?(epub_errfile)
+  # copy all epubs from done dir
+  # to coresource_send dir,
+  # which then triggers the coresource_connector.rb script in /utilities
+  filelist = getFileList(epubregexp, "files_to_copy")
+  copyFiles(filelist, csdir, "status")
+else
+  @loghash['copy_to_coresource_send_dir'] = '"EPUBCHECK_ERROR.txt" file found, skipping copy to coresource_send dir'
+end
 
 # Write json log:
 Mcmlln::Tools.logtoJson(@log_hash, 'completed', Time.now)
