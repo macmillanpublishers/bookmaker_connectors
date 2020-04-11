aa3cfc1d5abffabd3e57f20956d2640b9ab363a8
Matt Retzer <7025870+mattretzer@users.noreply.github.com>
2020-04-11 13:53:48 -0400

    1st full draft, handling rsv api

--- flask-api_execs/rsvalidate_exec.py
+++ flask-api_execs/rsvalidate_exec.py
@@ -1,48 +1,99 @@
+# this file accepts arguments from flask api and invokes rsvalidate process
+
 import sys
 import os
 import imp
+import platform
 import zipfile
+from datetime import datetime
+import logging
 import subprocess
+from textwrap import dedent
 from socket import gethostname
 from getpass import getuser
 
-# # # Define key paths dependencies
 
-# import arg definitions for extransoue params from flask, so we can pick them up by index
-exec_args_dirpath = os.path.join(sys.path[0], '..', '..', 'utilities', 'portable_flask-api', 'flaskr')
-exec_args = imp.load_source('exec_params', os.path.join(exec_args_dirpath,'exec_params.py'))
+# Define harcoded relative import paths from other repos
+utils_repo = os.path.join(sys.path[0], '..', '..', 'utilities')
+exec_args = imp.load_source('exec_params', os.path.join(utils_repo, 'portable_flask-api', 'flaskr', 'exec_params.py'))
+sendmail = imp.load_source('sendmail', os.path.join(utils_repo, 'python_utils','sendmail.py'))
 
 # # # Define key vars and params from input
 inputfile = sys.argv[1]
+# the relative order of these passed params is determined in flaskr/exec_params.py
 user_email = sys.argv[exec_args.execArgs().index('emailAddress') + 2]
 user_name = sys.argv[exec_args.execArgs().index('displayname') + 2]
 parentdir = os.path.dirname(inputfile)
 jobId = os.path.basename(parentdir)
 file_ext = os.path.splitext(inputfile)[1]
 
+# Other key definitions
+pypath = 'python'
+this_script = os.path.basename(sys.argv[0])
+runtype_string = 'direct'
 server = gethostname()
 currentuser = getuser()
+alert_emails_to = ['workflows@macmillan.com']
 staging_filename = 'staging.txt'
 staging_file = os.path.join("C:", os.sep, staging_filename)
-bkmkr_scripts_dir = os.path.join("S:", os.sep, "resources", "bookmaker_scripts")
+dropfolder_maindir = os.path.join("G:", os.sep, "My Drive", "Workflow Tools")    #<< drive
 # edits to above ^ for Mac OS / UNIX
 if platform.system() != 'Windows':  # for testing:
+    pypath = os.path.join(os.sep, 'usr', 'bin', 'python') # <- python 2.7 (system)
     staging_file = os.path.join(os.sep,"Users", currentuser, staging_filename)
+    dropfolder_maindir = os.path.join(os.sep, "Volumes", "GoogleDrive", "My Drive", "Workflow Tools")     #<< drive
     bkmkr_scripts_dir = os.path.join(os.sep,'Users', currentuser, 'bookmaker-dev')
+# conditional dependent paths
+logdir = os.path.join(dropfolder_maindir, "bookmaker_logs", "bookmaker_connectors", "api_execs")
 rsvalidate_cmd = os.path.join(bkmkr_scripts_dir, "sectionstart_converter", "xml_docx_stylechecks", "rsuitevalidate_main.py")
+# \/ probably don't need to implement process_watch.py for this app; since it already sends alerts on error
+#process_watch_py = os.path.join(bkmkr_scripts_dir, "sectionstart_converter", "xml_docx_stylechecks", "shared_utils", "process_watch.py")
+if os.path.exists(staging_file):
+    # bkmkr_dir = '{}_stg'.format(bkmkr_dir)
+    logdir = '{}_stg'.format(logdir)
+logfile = os.path.join(logdir, "{}-{}.log".format(this_script, datetime.now().strftime("%y-%m")))
 
 
-print ("inputfile: {}".format(inputfile))
-print ("jobId: {}".format(jobId))
-print ("user_email: {}".format(user_email))
-print ("user_name: {}".format(user_name))
+# # # ERROR ALERTS info:
+err_subject = "ALERT: {}: unexpected error ({})".format(this_script, server)
+err_txt  = dedent("""
+    Hello honored workflows personnel,
 
-# # # setup logging
+    An unexpected error was encountered while running {this_script}.
+    - server/host: {server}
+    - logfile: {logfile}
+    - errstring: {errstring}
+    """)
+err_dict = {
+    "err_subject": err_subject,
+    "err_txt": err_txt,
+    "alert_emails_to": alert_emails_to,
+    "server": server,
+    "logile": logfile,
+    "this_script": this_script
+}
 
 
 # # # FUNCTIONS
+def sendExceptionAlert(errstring, err_dict):
+    try:
+        logging.info("err occurred, attempting send ")
+        err_body = err_txt.format(server=err_dict['server'], logfile=err_dict['logfile'], \
+            this_script=err_dict['this_script'], errstring=errstring)
+        sendmail.sendMail(err_dict['alert_emails_to'], err_dict['err_subject'], err_dict['err_body'])
+    except Exception as e:
+        logging.error('Error sending Exception alert-mail', exc_info=True)
+
+def try_create_dir(newpath, err_dict):
+    try:
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+    except Exception as e:
+        logging.error('Destination dir "{}" could not be created'.format(newpath), exc_info=True)
+        sendExceptionAlert(e, err_dict)
+
 # handle unzipping as needed
-def unzipZips(inputfile):
+def unzipZips(inputfile, err_dict):
     if os.path.splitext(inputfile)[1] == '.zip':
         logging.debug("zip file, unzipping")
         parentdir = os.path.dirname(inputfile)
@@ -51,36 +102,45 @@ def unzipZips(inputfile):
             zf.extractall(parentdir)
             zf.close()
         except Exception as e:
-            # on error pass errstring back to parent script
-            print (e)
-            # return ''
+            logging.error('Error unzipping "{}"'.format(inputfile), exc_info=True)
+            sendExceptionAlert(e, err_dict)
 
-def invokeRSvalidate(rsvalidate_cmd, file, user_email, user_name):
-    print ("invoking for : {}, {}, {}".format(file, user_email, user_name))
+# kickoff process via subprocess.popen
+def invokeRSvalidate(pypath, cmd, file, runtype_string, user_email, user_name, err_dict):
+    p = ''
     try:
-        popen_params = ['python', r'{}'.format(os.path.join(rsvalidate_cmd)), file, user_email, user_name]
-        # logging.debug("popen params to launch bkmkr: \n'%s'" % popen_params)
+        popen_params = [pypath, r'{}'.format(os.path.join(cmd)), file, runtype_string, user_email, user_name]
+        logging.debug("popen params for 'invokeRSvalidate': \n{}".format(popen_params))
         p = subprocess.Popen(popen_params)
-        print p
-        # logging.info("bookmaker initiated for file '%s', pid %s" % (docx_object['name'], p.pid))
-    except Exception:
-        logging.error("error invoking bkmkr_cmd: '%s'" % bkmkr_cmd, exc_info=True)
-        # raise Exception('reraise')   # re-raise to send email
+        logging.info("rsvalidate initiated for file '%s', pid %s" % (os.path.basename(file), p.pid))
+        # logging.debug("popen output: {}".format(p)) < I don' think this works, b/c we are not piping stdout or anything
+    except Exception as e:
+        logging.error("error invoking bkmkr_cmd: '%s'" % cmd, exc_info=True)
+        if p:
+            logging.info("(popen output: {})".format(p))
+        sendExceptionAlert(e, err_dict)
+
+
+# # # setup logging
+# create log dir if it does not exist
+try_create_dir(logdir, err_dict)
+logging.basicConfig(filename=logfile, level=logging.DEBUG)#INFO)
+logging.info("* * * * * * running '{}':  {}".format(this_script, datetime.now().strftime("%y-%m-%d_%H:%M:%S")))
+
 
 # # # RUN
 if __name__ == '__main__':
-    # unzip file if it was a zip, into the parentdir
-    unzipZips(inputfile)
+    try:
+        # unzip file if it was a zip, into the parentdir
+        unzipZips(inputfile, err_dict)
+        # we're only looking for docx that were zipped solo here; so just checking for docx in
+        #   parentdir (where zips where extracted to) should be fine (no need to recurse)
+        for fname in os.listdir(parentdir):
+            if os.path.splitext(fname)[1] == '.docx':
+                file = os.path.join(parentdir, fname)
+                logging.debug('found docx: {}'.format(file))
+                output = invokeRSvalidate(pypath, rsvalidate_cmd, file, runtype_string, user_email, user_name, err_dict)
 
-    # we're only looking for docx that were zipped solo here; so just checking for docx in
-    #   parentdir (where zips where extracted to) should be fine (no need to recurse)
-    for fname in os.listdir(parentdir):
-        if os.path.splitext(fname)[1] == '.docx':
-            file = os.path.join(parentdir, fname)
-            output = invokeRSvalidate(file, user_email, user_name)
-            print output
-
-# logging. parameter handling. process watcher management. erreor mail. test python version here vs server.
-
-# will need to override dbox params in rsvalidate; may be a little tricky.
-# But can define args by script, so shouls not be bad; and can force disable dropbox based on executable.
+    except Exception as e:
+        logging.error("untrapped top-level exception occurred", exc_info=True)
+        sendExceptionAlert(e, err_dict)
