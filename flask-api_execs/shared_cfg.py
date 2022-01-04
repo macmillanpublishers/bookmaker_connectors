a76e7711f9aa5fa1799832332b79cfb46d6d8f03
Matt Retzer <7025870+mattretzer@users.noreply.github.com>
2022-01-04 13:57:59 -0500

    comment update

--- flask-api_execs/shared_cfg.py
+++ flask-api_execs/shared_cfg.py
@@ -31,11 +31,11 @@ file_ext = os.path.splitext(inputfile)[1]
 infile_name = os.path.basename(inputfile)
 
 # Other key definitions
-pypath = 'python'   # <- use default installed python from PATH
-# # \/ use specified python binary from hardcoded path
+pypath = 'python'   # <- use default installed python from PATH, or ...
+# # ... use specified python binary from hardcoded path \/
 # pypath = os.path.join('C:', os.sep, 'Python27', 'python.exe')
-rubypath = 'ruby'   # <- use default installed ruby from PATH
-# # \/ use specified ruby binary from hardcoded path
+rubypath = 'ruby'   # <- use default installed ruby from PATH, or ...
+# # ... use specified ruby binary from hardcoded path \/
 # rubypath = os.path.join('C:', os.sep, 'Ruby200', 'bin', 'ruby.exe')
 this_script = os.path.basename(sys.argv[0])
 runtype_string = 'direct'
