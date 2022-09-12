99df423d76ae100e55c696c22c60edfee4ad1b48
Matt Retzer <7025870+mattretzer@users.noreply.github.com>
2022-09-12 14:36:42 -0400

    debug rnd 1

--- api_POST_to_flask.py
+++ api_POST_to_flask.py
@@ -3,21 +3,8 @@ import os
 import requests
 from requests.auth import HTTPBasicAuth
 
-# accepting x number of optional parameter k:v pairs from the command line, like so:
-#   python filepath/filename host_url key1 value1 key2 value2 ...
-# if basic auth is not enabled on host you can use dummy values for user and pass, they are ignored
-file = sys.argv[1]
-url_POST = sys.argv[2]
-uname = sys.argv[3]
-pw = sys.argv[4]
-params = {}
-if len(sys.argv) > 5:
-    for i in list(range(5, len(sys.argv)-1)):
-        if(i%2 == 1):   # if param # is odd it's a param key
-            params[sys.argv[i]]=sys.argv[i+1]
-
 # send POST request with file
-def apiPOST(file, url_POST, params, uname, pw):
+def apiPOST(file, url_POST, uname, pw, params):
     try:
         filename = os.path.basename(file)
         fileobj = open(file,'rb')
@@ -34,5 +21,18 @@ def apiPOST(file, url_POST, params, uname, pw):
         return 'error: {}'.format(e)#
 
 if __name__ == '__main__':
-    resultstr = apiPOST(file, url_POST, params, uname, pw)
+    # accepting x number of optional parameter k:v pairs from the command line, like so:
+    #   python filepath/filename host_url key1 value1 key2 value2 ...
+    # if basic auth is not enabled on host you can use dummy values for user and pass, they are ignored
+    file = sys.argv[1]
+    url_POST = sys.argv[2]
+    uname = sys.argv[3]
+    pw = sys.argv[4]
+    params = {}
+    if len(sys.argv) > 5:
+        for i in list(range(5, len(sys.argv)-1)):
+            if(i%2 == 1):   # if param # is odd it's a param key
+                params[sys.argv[i]]=sys.argv[i+1]
+
+    resultstr = apiPOST(file, url_POST, uname, pw, params)
     print (resultstr)
